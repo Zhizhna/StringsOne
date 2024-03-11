c27a32ce5f63ca67b94759ea1b56949c0a78c440
govor <govorinsasha0@gmail.com>
2024-03-11 09:55:56 +0100

    All done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника —" + fullName);
+        String fullNameForReport = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —" + fullNameForReport);
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
